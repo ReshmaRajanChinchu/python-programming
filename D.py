cf5b1918bdc3693755ab3526ac2ecb508239facf
ReshmaRajanChinchu <41043283+ReshmaRajanChinchu@users.noreply.github.com>
2018-09-17 11:32:20 +0530

    Add files via upload

---
+++ D.py
@@ -0,0 +1,8 @@
+l = input("Enter the letter:")
+if l =='0':
+    exit();
+else:
+    if l in ('a', 'e', 'i', 'o', 'u'):
+    	print(l,"is an alphabet.");
+    else:
+    	print(l,"is not an alphabet.");
