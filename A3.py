3e732356c1d723edb58c797f017879d342de697c
ReshmaRajanChinchu <41043283+ReshmaRajanChinchu@users.noreply.github.com>
2018-09-26 14:20:12 +0530

    Add files via upload

---
+++ A3.py
@@ -0,0 +1,10 @@
+x=int(input("Enter the alphabet:")
+list=["a","b","c","d","e","A","B","C","D","E"]
+x=raw_input("")
+print(x)
+
+if (x in list):
+    print (" Vowel")
+
+else:
+    print ("consonant")
